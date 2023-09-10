633d82229bfb99dc8179c06111616ca29c1f6f6a
Sourabh Kumar <inspiron3593_8MT6S53@outlook.com>
2023-09-10 12:19:01 +0530

    Upload social Media API

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import router from "./routes/user-routes";
+import blogRouter from "./routes/blog-routes";
+import dotenv from "dotenv";
+dotenv.config();
+
+const uri = process.env.MONGO_URL;
+const app = express();
+app.use(express.json());
+app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(process.env.PORT))
+  .then(() =>
+    console.log("Connected to database and listening to localhost 5000")
+  )
+  .catch((err) => console.log(err));
